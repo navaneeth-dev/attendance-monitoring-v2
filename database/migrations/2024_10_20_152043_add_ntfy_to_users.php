97b2893e9f552acb8d0d0f0a6c223198eec58c38
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-10-24 15:41:47 +0530

    feat: ntfy_channel column

--- database/migrations/2024_10_20_152043_add_ntfy_to_users.php
+++ database/migrations/2024_10_20_152043_add_ntfy_to_users.php
@@ -8,14 +8,25 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
+            $table->ulid('ntfy_channel')->nullable();
+        });
 
+        $users = \App\Models\User::all();
+        foreach ($users as $user) {
+            $user->ntfy_channel = Str::ulid();
+            $user->save();
+            Log::info($user->ntfy_channel);
+        }
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->ulid('ntfy_channel')->unique()->change();
         });
     }
 
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('ntfy_channel');
         });
     }
 };
